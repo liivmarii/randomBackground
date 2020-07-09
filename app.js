22a0a4de561b757b1eb819a08f056937f405e57c
liivmarii <liv.alterskjaer@icloud.com>
2020-07-09 23:05:39 +0200

    Code snippet text nodes update with gradient change

--- app.js
+++ app.js
@@ -4,6 +4,41 @@ document.addEventListener('DOMContentLoaded', () => {
     const background = document.querySelector('#gradient')
     const colorBtn = document.querySelector('#generate-gradient-btn')
 
+    // Default first & second colour
+    let firstColor = 'rgba(100, 100, 100, 1)'
+    let secondColor = 'rgba(210, 210, 210, 1)'
+
+    let previousGradient
+    let currentGradient = 'linear-gradient(62deg, ' + firstColor + ' 0%, ' + secondColor + ' 100%)'
+    let gradients = []
+
+    // DOM text nodes for code snippet
+    const mozGradient = document.querySelector('#moz-linear-gradient')
+    const webkitGradient = document.querySelector('#webkit-linear-gradient')
+    const defaultGradient = document.querySelector('#linear-gradient')
+
+    let mozNode = document.createTextNode('-moz-' + currentGradient)
+    let webkitNode = document.createTextNode('-webkit-' + currentGradient)
+    let defaultNode = document.createTextNode(currentGradient)
+
+    mozGradient.appendChild(mozNode)
+    webkitGradient.appendChild(webkitNode)
+    defaultGradient.appendChild(defaultNode)
+
+    function updateTextNodes() {
+        mozGradient.removeChild(mozNode)
+        webkitGradient.removeChild(webkitNode)
+        defaultGradient.removeChild(defaultNode)
+
+        mozNode = document.createTextNode('-moz-' + currentGradient)
+        webkitNode = document.createTextNode('-webkit-' + currentGradient)
+        defaultNode = document.createTextNode(currentGradient)
+
+        mozGradient.appendChild(mozNode)
+        webkitGradient.appendChild(webkitNode)
+        defaultGradient.appendChild(defaultNode)
+    }
+
     // Create random gradient degree
     let randomDeg = () => Math.floor(Math.random() * 360)
 
@@ -12,14 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
     let opacity = 1
     let rgbValue = () => Math.floor(Math.random() * rgbMax)
     let randomRgba = () => 'rgba(' + rgbValue() + ', ' + rgbValue() + ', ' + rgbValue() + ', ' + opacity + ')'
-
-    // Default first & second colour
-    let firstColor = 'rgba(100, 100, 100, 1)'
-    let secondColor = 'rgba(210, 210, 210, 1)'
-
-    let previousGradient
-    let currentGradient
-    let gradients = []
 
     function colorMatch() {
         firstColor = randomRgba()
@@ -41,8 +68,9 @@ document.addEventListener('DOMContentLoaded', () => {
         gradients.push(currentGradient)
 
         background.style.backgroundImage = currentGradient
-
         colorBtn.style.color = firstColor
+
+        updateTextNodes()
 
     }
 
