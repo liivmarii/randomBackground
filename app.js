f4a4906fa0e44c998f4fc6fede8d339e7f159e6b
liivmarii <liv.alterskjaer@icloud.com>
2020-07-09 17:49:08 +0200

    Base for gradient colour change on click

---
+++ app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // DOM content
+    const colorContainer = document.querySelector('#bg-container')
+    const colorBtn = document.querySelector('#color-btn')
+
+    const colors = ['yellow', 'red', 'green', 'blue']
+    let leftColor
+    let rightColor
+
+    let previousGradient
+    let currentGradient
+    let gradients = []
+
+    // Change background on click
+    colorBtn.addEventListener('click', updateGradient)
+
+    function random(array) {
+        let randomIndex = Math.floor(Math.random() * array.length)
+        return randomIndex
+    }
+
+    function colorMatch() {
+        leftColor = colors[random(colors)]
+        rightColor = colors[random(colors)]
+        while (leftColor === rightColor) rightColor = colors[random(colors)]
+    }
+
+    function updateGradient() {
+
+        colorMatch()
+        previousGradient = gradients[gradients.length - 1]
+        currentGradient = 'linear-gradient(62deg, ' + leftColor + ' 0%, ' + rightColor + ' 100%)'
+
+        while (previousGradient === currentGradient) {
+            colorMatch()
+            currentGradient = 'linear-gradient(62deg, ' + leftColor + ' 0%, ' + rightColor + ' 100%)'
+        }
+
+        colorContainer.style.backgroundImage = currentGradient
+        gradients.push(currentGradient)
+
+        console.log('colorcombo: ' + leftColor + ' + ' + rightColor)
+        console.log('previousGradient: ' + previousGradient)
+        console.log('currentGradient: ' + currentGradient)
+        console.log(gradients)
+
+    }
+
+
+})
